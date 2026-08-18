c35fa6b2b481271cea1d934774d6e0e46bfc4647
Jonny Collins <jonny.collins111@gmail.com>
2026-08-18 09:24:34 +0100

    Added imgui dependency, setup test client GUI

--- SyncSlateClient/SyncSlateClient.cpp
+++ SyncSlateClient/SyncSlateClient.cpp
@@ -1,10 +1,109 @@
 #include "SyncSlateClient.h"
-void SyncSlateClient::start(){
+#include <SDL3/SDL.h>
+#include <imgui.h>
+#include <imgui_impl_sdl3.h>
+#include <imgui_impl_sdlrenderer3.h>
+#include <cmath>
+
+void SyncSlateClient::start() {
 
 }
 
-
 int main(int argc, char* argv[]) {
-	// ...
-	return 0;
+    // --- Setup: SDL window + renderer ---
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("SDL_Init failed: %s", SDL_GetError());
+        return -1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("SyncSlate", 1280, 720, SDL_WINDOW_RESIZABLE);
+    if (!window) {
+        SDL_Log("SDL_CreateWindow failed: %s", SDL_GetError());
+        return -1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+    if (!renderer) {
+        SDL_Log("SDL_CreateRenderer failed: %s", SDL_GetError());
+        return -1;
+    }
+
+    // --- Setup: ImGui context + backends ---
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer3_Init(renderer);
+
+    bool my_tool_active = true;
+    float my_color[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
+
+    // --- Main loop ---
+    bool running = true;
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            ImGui_ImplSDL3_ProcessEvent(&event);
+            if (event.type == SDL_EVENT_QUIT) {
+                running = false;
+            }
+        }
+
+        // Start the frame
+        ImGui_ImplSDLRenderer3_NewFrame();
+        ImGui_ImplSDL3_NewFrame();
+        ImGui::NewFrame();
+
+        // --- UI ---
+        if (my_tool_active) {
+            ImGui::Begin("My First Tool", &my_tool_active, ImGuiWindowFlags_MenuBar);
+
+            if (ImGui::BeginMenuBar()) {
+                if (ImGui::BeginMenu("File")) {
+                    if (ImGui::MenuItem("Open..", "Ctrl+O")) { /* Do stuff */ }
+                    if (ImGui::MenuItem("Save", "Ctrl+S")) { /* Do stuff */ }
+                    if (ImGui::MenuItem("Close", "Ctrl+W")) { my_tool_active = false; }
+                    ImGui::EndMenu();
+                }
+                ImGui::EndMenuBar();
+            }
+
+            ImGui::ColorEdit4("Color", my_color);
+
+            static float samples[100];
+            for (int n = 0; n < 100; n++)
+                samples[n] = sinf(n * 0.2f + (float)ImGui::GetTime() * 1.5f);
+            ImGui::PlotLines("Samples", samples, 100);
+
+            ImGui::TextColored(ImVec4(1, 1, 0, 1), "Important Stuff");
+            ImGui::BeginChild("Scrolling");
+            for (int n = 0; n < 50; n++)
+                ImGui::Text("%04d: Some text", n);
+            ImGui::EndChild();
+
+            ImGui::End();
+        }
+
+        // --- Render ---
+        ImGui::Render();
+        SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
+        SDL_RenderClear(renderer);
+        ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    // --- Cleanup ---
+    ImGui_ImplSDLRenderer3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
